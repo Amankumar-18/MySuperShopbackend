ceb7524ee9f7fe3870e0c057face6984de3017b7
Amankumar-18 <amankumarsingh18114914@gmail.com>
2026-06-11 23:40:56 +0530

    Connected PostgreSQL database

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 from datetime import timedelta
 load_dotenv()
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
